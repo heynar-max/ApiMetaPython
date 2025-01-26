6a5dd9585ea4a88de5d05713b991e52c349f6897
heynar-max <heynar76@hotmail.com>
2025-01-26 17:35:16 -0500

    #22

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from controller import webhook
-from db import db
 
 app = Flask(__name__)
 
 # Configuración de la base de datos
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///metapython.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db.init_app(app)
+db = SQLAlchemy(app)
 
 # Crear la tabla si no existe
 with app.app_context():
